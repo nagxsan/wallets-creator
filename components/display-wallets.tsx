d1381602ef80529b8eebd93bef0ccd576602bf85
nagxsan <sanchetng@gmail.com>
2024-08-19 13:12:09 +0530

    fix delete wallet logic

--- components/display-wallets.tsx
+++ components/display-wallets.tsx
@@ -3,14 +3,27 @@ import { useRecoilState } from "recoil"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion"
 import { BlockchainType } from "@/constants/enums"
 import Image from "next/image"
-import { useState } from "react"
-import { Asterisk, Copy, Eye, EyeOff, Trash2 } from "lucide-react"
+import { useEffect, useState } from "react"
+import { Asterisk, Copy, Eye, EyeOff } from "lucide-react"
 import { toast } from "sonner"
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog"
+import { Button } from "./ui/button"
+import useScreenSize from "@/hooks/useSreenSize"
 
 export function DisplayWallets() {
 
+  const screenSize = useScreenSize()
+
   const [wallets, setWallets] = useRecoilState(walletsAtom)
-  const [viewPrivateKey, setViewPrivateKey] = useState<boolean[]>(new Array(wallets.length).fill(false))
+  const [viewPrivateKey, setViewPrivateKey] = useState<Map<string, boolean>>()
+
+  useEffect(() => {
+    const mapViewPrivateKey = new Map()
+    for (const wallet of wallets) {
+      mapViewPrivateKey.set(wallet.id, false)
+    }
+    setViewPrivateKey(mapViewPrivateKey)
+  }, [])
 
   const getBlockchainIcon = (blockchainType: BlockchainType) => {
     var asset = ''
@@ -25,11 +38,10 @@ export function DisplayWallets() {
     return <Image src={asset} alt={asset} width={24} height={24} />
   }
 
-  const handleViewPrivateKey = (idx: number, view: boolean) => {
-    const newViewPrivateKey = [...viewPrivateKey]
-    newViewPrivateKey[idx] = view
+  const handleViewPrivateKey = (id: string, view: boolean) => {
+    const newViewPrivateKey = new Map(viewPrivateKey)
+    newViewPrivateKey.set(id, view)
 
-    console.log(newViewPrivateKey)
     setViewPrivateKey(newViewPrivateKey)
   }
 
@@ -43,17 +55,16 @@ export function DisplayWallets() {
     )
   }
 
-  const handleDeleteWallet = (idx: number) => {
-    const updatedWallets = [...wallets]
-    const walletToDelete = wallets[idx]
-    updatedWallets.filter((wallet) => wallet.id !== walletToDelete.id)
+  const handleDeleteWallet = (id: string) => {
+    const updatedWallets = [...wallets].filter((wallet) => wallet.id !== id)
+    localStorage.setItem('wallets', JSON.stringify(updatedWallets))
     setWallets(updatedWallets)
   }
 
   return (
     <div className="flex flex-col gap-y-4">
-      {wallets.map((wallet, idx) => (
-        <Accordion key={idx} type="single" collapsible>
+      {wallets.map((wallet, _) => (
+        <Accordion key={wallet.id} type="single" collapsible>
           <AccordionItem value="item-1">
             <AccordionTrigger disableChevronDown={true}>
               <div className="flex items-center justify-between w-full">
@@ -61,7 +72,6 @@ export function DisplayWallets() {
                   {getBlockchainIcon(wallet.blockchainType)}
                   {wallet.name}
                 </div>
-                <Trash2 size={28} color="#e83b3b" onClick={() => handleDeleteWallet(idx)} />
               </div>
             </AccordionTrigger>
             <AccordionContent>
@@ -83,28 +93,59 @@ export function DisplayWallets() {
                       toast('Private Key copied to clipboard.')
                     }}
                   >
-                    Private Key: {viewPrivateKey[idx] ? wallet.privateKey : getMaskedPrivateKey(wallet.privateKey.length)}
+                    Private Key: {viewPrivateKey?.get(wallet.id) ? wallet.privateKey : getMaskedPrivateKey(wallet.privateKey.length)}
                   </div>
                   {
-                    viewPrivateKey[idx]
+                    viewPrivateKey?.get(wallet.id)
                     ?
                     <Eye
                       size={28}
                       className="hover:cursor-pointer"
-                      onClick={() => handleViewPrivateKey(idx, false)}
+                      onClick={() => handleViewPrivateKey(wallet.id, false)}
                     />
                     :
                     <EyeOff
                       size={28}
                       className="hover:cursor-pointer"
-                      onClick={() => handleViewPrivateKey(idx, true)}
+                      onClick={() => handleViewPrivateKey(wallet.id, true)}
                     />
                   }
                 </div>
               </div>
-              <div className="flex gap-x-3 pt-6 text-md">
+              <div className="flex gap-x-3 pt-6 text-md mb-5">
                 Click on key to copy to clipboard. <Copy size={20} />
               </div>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                <Button
+                  size={screenSize.width > 500 || screenSize.width === 0 ? "default" : "full"}
+                  variant="destructive"
+                  disabled={wallets.length === 0}
+                >
+                  Delete Wallet
+                </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you sure you want to delete the wallet?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently
+                      delete your wallet from local storage.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={() => handleDeleteWallet(wallet.id)}
+                      className="text-destructive"
+                    >
+                      Delete
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </AccordionContent>
           </AccordionItem>
         </Accordion>
